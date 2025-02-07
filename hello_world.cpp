aaba6c008048e011d6c360ff00019ccfc7a401ed
Artem-Darius Weber <mit.3tlasa@gmail.com>
2025-02-07 11:46:28 +0000

    Применён clang-format (стиль Mozilla)

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-    string name;
-    cout << "Введите имя: ";
-    cin >> name;
-    cout << "Hello world from " << name << endl;
-    return 0;
+int
+main()
+{
+  string name;
+  cout << "Введите имя: ";
+  cin >> name;
+  cout << "Hello world from " << name << endl;
+  return 0;
 }
-
